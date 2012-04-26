6b5a7270cfdd1989e2c7efdd9651d04bb42f78dc
joshglass <josh@softwareallies.com>
2012-04-26 14:48:14 -0500

    Update lib/salesforce_bulk/connection.rb

--- lib/salesforce_bulk/connection.rb
+++ lib/salesforce_bulk/connection.rb
@@ -61,10 +61,10 @@ module SalesforceBulk
 
       if host != @@LOGIN_HOST # Not login, need to add session id to header
         headers['X-SFDC-Session'] = @session_id;
-          puts "session id is: #{@session_id} --- #{headers.inspect}\n"
+          # puts "session id is: #{@session_id} --- #{headers.inspect}\n"
         path = "#{@@PATH_PREFIX}#{path}"
       end
-        puts "#{host} -- #{path} -- #{headers.inspect}\n"
+      #  puts "#{host} -- #{path} -- #{headers.inspect}\n"
       https(host).post(path, xml, headers).body
     end
 
