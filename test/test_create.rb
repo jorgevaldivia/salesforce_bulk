afe2343182785d532ae006f180faa94d7d8435ce
Javier Julio <jjfutbol@gmail.com>
2012-05-25 19:28:25 -0400

    Add a sample test case just to verify that we have tests running successfully when installing the gem.

---
+++ test/test_create.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class TestCreate < Test::Unit::TestCase
+  
+  test "should run this test successfully" do
+    assert_equal 1, 1
+    assert_equal "test", "test"
+  end
+  
+end
