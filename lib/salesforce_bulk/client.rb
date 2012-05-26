8eb32b2a002664c83e4a69b4846e7f622ad546dc
Javier Julio <jjfutbol@gmail.com>
2012-05-25 21:16:36 -0400

    Update client to pass all tests and change the merge so it uses the defaults properly.

--- lib/salesforce_bulk/client.rb
+++ lib/salesforce_bulk/client.rb
@@ -1,7 +1,10 @@
 module SalesforceBulk
   # Interface for operating the Salesforce Bulk REST API
   class Client
-    # The host to use for authentication. Defaults to login.salesforce.com
+    # If true, print API debugging information to stdout. Defaults to false.
+    attr_accessor :debugging
+    
+    # The host to use for authentication. Defaults to login.salesforce.com.
     attr_accessor :host
     
     # The Salesforce password
@@ -13,7 +16,7 @@ module SalesforceBulk
     # The Salesforce username
     attr_accessor :username
     
-    # The API version the client is using. Defaults to 23.0
+    # The API version the client is using. Defaults to 23.0.
     attr_accessor :version
     
     def initialize(options={})
@@ -21,8 +24,9 @@ module SalesforceBulk
       self.password = options[:password]
       self.token = options[:token]
       
-      options.merge!(:host => 'login.salesforce.com', :version => '23.0')
+      options = {:debugging => false, :host => 'login.salesforce.com', :version => '23.0'}.merge(options)
       
+      self.debugging = options[:debugging]
       self.host = options[:host]
       self.version = options[:version]
     end
