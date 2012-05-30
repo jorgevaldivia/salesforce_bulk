f3743964bd672e8e8fc36e332d8d398c873505f4
Javier Julio <jjfutbol@gmail.com>
2012-05-30 00:17:40 -0400

    Update test_job class with a new test for making sure a job.status method returns a job info detail document.

--- test/lib/test_job.rb
+++ test/lib/test_job.rb
@@ -87,4 +87,25 @@ class TestJob < Test::Unit::TestCase
     assert_equal job.state, 'Aborted'
   end
   
+  test "should return job info" do
+    bypass_authentication(@client)
+    
+    headers = {'Content-Type' => 'application/xml', 'X-Sfdc-Session' => '123456789'}
+    response = fixture("job_info_response.xml")
+    jobId = "750E00000004N1mIAE"
+    
+    stub_request(:get, "#{api_url(@client)}job/#{jobId}")
+      .with(:body => '', :headers => headers)
+      .to_return(:body => response, :status => 200)
+    
+    job = @client.job(:upsert, :VideoEvent__c, :Id__c)
+    job.instance_variable_set("@id", jobId)
+    info = job.status
+    
+    assert_requested :get, "#{api_url(@client)}job/#{jobId}", :body => '', :headers => headers, :times => 1
+    
+    assert_equal job.id, jobId
+    assert_equal job.state, 'Open'
+  end
+  
 end
