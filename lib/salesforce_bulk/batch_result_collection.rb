1b71fef5fd610566b89c958709ea1f613ff9ff72
Javier Julio <jjfutbol@gmail.com>
2012-06-01 02:52:13 -0400

    Add first draft of BatchResultCollection class.

---
+++ lib/salesforce_bulk/batch_result_collection.rb
@@ -0,0 +1,36 @@
+module SalesforceBulk
+  class BatchResultCollection < Array
+    
+    attr_reader :client
+    attr_reader :currentIndex
+    attr_reader :batchId
+    attr_reader :jobId
+    attr_reader :resultIds
+    
+    def initialize(client, jobId, batchId, resultIds) #previousResultId, nextResultId, currentResultId
+      @client = client
+      @jobId = jobId
+      @batchId = batchId
+      @resultIds = resultIds
+      @currentIndex = resultIds.first
+      
+    end
+    
+    def next?
+      
+    end
+    
+    def next
+      
+    end
+    
+    def previous?
+      
+    end
+    
+    def previous
+      
+    end
+    
+  end
+end
