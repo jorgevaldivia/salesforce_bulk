669ed0ea8749cf2df4942b823e31699216eb65ab
Javier Julio <jjfutbol@gmail.com>
2012-06-02 01:20:09 -0400

    Remove inline headers definition in each test and define in the setup method as its used with the same value across all.

--- test/lib/test_job.rb
+++ test/lib/test_job.rb
@@ -10,6 +10,7 @@ class TestJob < Test::Unit::TestCase
     }
     
     @client = SalesforceBulk::Client.new(options)
+    @headers = {'Content-Type' => 'application/xml', 'X-Sfdc-Session' => '123456789'}
   end
   
   test "should return initialized job object" do
@@ -26,17 +27,16 @@ class TestJob < Test::Unit::TestCase
   test "should create job and return successful response" do
     bypass_authentication(@client)
     
-    headers = {'Content-Type' => 'application/xml', 'X-Sfdc-Session' => '123456789'}
     request = fixture("job_create_request.xml")
     response = fixture("job_create_response.xml")
     
     stub_request(:post, "#{api_url(@client)}job")
-      .with(:body => request, :headers => headers)
+      .with(:body => request, :headers => @headers)
       .to_return(:body => response, :status => 200)
     
     job = @client.add_job(:operation => :upsert, :sobject => :VideoEvent__c, :externalIdFieldName => :Id__c)
     
-    assert_requested :post, "#{api_url(@client)}job", :body => request, :headers => headers, :times => 1
+    assert_requested :post, "#{api_url(@client)}job", :body => request, :headers => @headers, :times => 1
     
     assert_equal job.id, '750E00000004MzbIAE'
     assert_equal job.state, 'Open'
@@ -45,18 +45,17 @@ class TestJob < Test::Unit::TestCase
   test "should close job and return successful response" do
     bypass_authentication(@client)
     
-    headers = {'Content-Type' => 'application/xml', 'X-Sfdc-Session' => '123456789'}
     request = fixture("job_close_request.xml")
     response = fixture("job_close_response.xml")
     jobId = "750E00000004MzbIAE"
     
     stub_request(:post, "#{api_url(@client)}job/#{jobId}")
-      .with(:body => request, :headers => headers)
+      .with(:body => request, :headers => @headers)
       .to_return(:body => response, :status => 200)
     
     job = @client.close_job(jobId)
     
-    assert_requested :post, "#{api_url(@client)}job/#{jobId}", :body => request, :headers => headers, :times => 1
+    assert_requested :post, "#{api_url(@client)}job/#{jobId}", :body => request, :headers => @headers, :times => 1
     
     assert_equal job.id, jobId
     assert_equal job.state, 'Closed'
@@ -65,18 +64,17 @@ class TestJob < Test::Unit::TestCase
   test "should abort job and return successful response" do
     bypass_authentication(@client)
     
-    headers = {'Content-Type' => 'application/xml', 'X-Sfdc-Session' => '123456789'}
     request = fixture("job_abort_request.xml")
     response = fixture("job_abort_response.xml")
     jobId = "750E00000004N1NIAU"
     
     stub_request(:post, "#{api_url(@client)}job/#{jobId}")
-      .with(:body => request, :headers => headers)
+      .with(:body => request, :headers => @headers)
       .to_return(:body => response, :status => 200)
     
     job = @client.abort_job(jobId)
     
-    assert_requested :post, "#{api_url(@client)}job/#{jobId}", :body => request, :headers => headers, :times => 1
+    assert_requested :post, "#{api_url(@client)}job/#{jobId}", :body => request, :headers => @headers, :times => 1
     
     assert_equal job.id, jobId
     assert_equal job.state, 'Aborted'
@@ -85,17 +83,16 @@ class TestJob < Test::Unit::TestCase
   test "should return job info" do
     bypass_authentication(@client)
     
-    headers = {'Content-Type' => 'application/xml', 'X-Sfdc-Session' => '123456789'}
     response = fixture("job_info_response.xml")
     jobId = "750E00000004N1mIAE"
     
     stub_request(:get, "#{api_url(@client)}job/#{jobId}")
-      .with(:body => '', :headers => headers)
+      .with(:body => '', :headers => @headers)
       .to_return(:body => response, :status => 200)
     
     job = @client.job_info(jobId)
     
-    assert_requested :get, "#{api_url(@client)}job/#{jobId}", :body => '', :headers => headers, :times => 1
+    assert_requested :get, "#{api_url(@client)}job/#{jobId}", :body => '', :headers => @headers, :times => 1
     
     assert_equal job.id, jobId
     assert_equal job.state, 'Open'
