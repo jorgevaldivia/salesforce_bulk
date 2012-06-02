8b9625dd0d51d201032962f3faa347a6c7add432
Javier Julio <jjfutbol@gmail.com>
2012-06-02 16:54:12 -0400

    Update properties in BatchResult class to be attr_accessor instead.

--- lib/salesforce_bulk/batch_result.rb
+++ lib/salesforce_bulk/batch_result.rb
@@ -2,16 +2,16 @@ module SalesforceBulk
   class BatchResult
     
     # A boolean indicating if record was created. If updated value is false.
-    attr_reader :created
+    attr_accessor :created
     
     # The error message.
-    attr_reader :error
+    attr_accessor :error
     
     # The record's unique id.
-    attr_reader :id
+    attr_accessor :id
     
     # If record was created successfully. If false then an error message is provided. 
-    attr_reader :success
+    attr_accessor :success
     
     def initialize(attrs={})
       
