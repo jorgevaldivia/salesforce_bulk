0233b72a71d7f13679fb4fa6fe01479496ae40b6
Javier Julio <jjfutbol@gmail.com>
2012-06-01 23:41:28 -0400

    Remove create, close abort and info methods as we'll be defining these instead on the client object.

--- lib/salesforce_bulk/job.rb
+++ lib/salesforce_bulk/job.rb
@@ -25,43 +25,6 @@ module SalesforceBulk
       end
     end
     
-    def create
-      #@client.add_job()
-    end
-    
-    def close
-      xml = '<?xml version="1.0" encoding="utf-8"?>'
-      xml += '<jobInfo xmlns="http://www.force.com/2009/06/asyncapi/dataload">'
-      xml += "<state>Closed</state>"
-      xml += "</jobInfo>"
-      
-      #puts "","",xml
-      response = @client.http_post("job/#{id}", xml)
-      data = XmlSimple.xml_in(response.body)
-      #puts "","",response
-      @state = data['state'][0]
-    end
-    
-    def abort
-      xml = '<?xml version="1.0" encoding="utf-8"?>'
-      xml += '<jobInfo xmlns="http://www.force.com/2009/06/asyncapi/dataload">'
-      xml += "<state>Aborted</state>"
-      xml += "</jobInfo>"
-      
-      #puts "","",xml
-      response = @client.http_post("job/#{id}", xml)
-      data = XmlSimple.xml_in(response.body)
-      #puts "","",response
-      @state = data['state'][0]
-    end
-    
-    def info
-      response = @client.http_get("job/#{id}")
-      data = XmlSimple.xml_in(response.body)
-      #puts "","",response
-      @state = data['state'][0]
-    end
-    
     def batch(data)
       batch = Batch.new(@client)
       batch.data = data
