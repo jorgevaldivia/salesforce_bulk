10c38c348d0a4c161fa39d0bda4fe681429bc801
Javier Julio <jjfutbol@gmail.com>
2012-06-02 00:17:18 -0400

    Remove old method implementations from the previous version since we now have working ones with tests all implemented in the client object. All that remains is batch_info_list and batch_results.

--- lib/salesforce_bulk/job.rb
+++ lib/salesforce_bulk/job.rb
@@ -25,56 +25,13 @@ module SalesforceBulk
       end
     end
     
-    def batch(data)
-      batch = Batch.new(@client)
-      batch.data = data
-      batch.create(self)
-    end
-    
-    def batch_status
+    def batch_info_list
       response = @client.http_get("job/#{id}/batch")
       data = XmlSimple.xml_in(response.body)
       puts "","",response,""
       #@state = data['state'][0]
     end
     
-    
-    
-    
-    def create_job()
-      xml = '<?xml version="1.0" encoding="utf-8"?>'
-      xml += "<jobInfo xmlns=\"http://www.force.com/2009/06/asyncapi/dataload\">"
-      xml += "<operation>#{@@operation}</operation>"
-      xml += "<object>#{@@sobject}</object>"
-      if !@@external_field.nil? # This only happens on upsert
-        xml += "<externalIdFieldName>#{@@external_field}</externalIdFieldName>"
-      end
-      xml += "<contentType>CSV</contentType>"
-      xml += "</jobInfo>"
-
-      path = "job"
-      headers = Hash['Content-Type' => 'application/xml; charset=utf-8']
-
-      response = @@connection.post_xml(nil, path, xml, headers)
-      response_parsed = XmlSimple.xml_in(response)    
-
-      @@job_id = response_parsed['id'][0]
-    end
-
-    def close_job()
-      xml = "#{@@XML_HEADER}<jobInfo xmlns=\"http://www.force.com/2009/06/asyncapi/dataload\">"
-      xml += "<state>Closed</state>"
-      xml += "</jobInfo>"
-
-      path = "job/#{@@job_id}"
-      headers = Hash['Content-Type' => 'application/xml; charset=utf-8']
-
-      response = @@connection.post_xml(nil, path, xml, headers)
-      response_parsed = XmlSimple.xml_in(response)
-
-      #job_id = response_parsed['id'][0]
-    end
-
     def add_query
       path = "job/#{@@job_id}/batch/"
       headers = Hash["Content-Type" => "text/csv; charset=UTF-8"]
@@ -84,49 +41,7 @@ module SalesforceBulk
 
       @@batch_id = response_parsed['id'][0]
     end
-
-    def add_batch()
-      keys = @@records.reduce({}) {|h,pairs| pairs.each {|k,v| (h[k] ||= []) << v}; h}.keys
-      headers = keys.to_csv
-      
-      output_csv = headers
-
-      @@records.each do |r|
-        fields = Array.new
-        keys.each do |k|
-          fields.push(r[k])
-        end
-
-        row_csv = fields.to_csv
-        output_csv += row_csv
-      end
-
-      path = "job/#{@@job_id}/batch/"
-      headers = Hash["Content-Type" => "text/csv; charset=UTF-8"]
-      
-      response = @@connection.post_xml(nil, path, output_csv, headers)
-      response_parsed = XmlSimple.xml_in(response)
-
-      @@batch_id = response_parsed['id'][0]
-    end
-
-    def check_batch_status()
-      path = "job/#{@@job_id}/batch/#{@@batch_id}"
-      headers = Hash.new
-
-      response = @@connection.get_request(nil, path, headers)
-      response_parsed = XmlSimple.xml_in(response)
-
-      begin
-        #puts "check: #{response_parsed.inspect}\n"
-        response_parsed['state'][0]
-      rescue Exception => e
-        #puts "check: #{response_parsed.inspect}\n"
-
-        nil
-      end
-    end
-
+    
     def get_batch_result()
       path = "job/#{@@job_id}/batch/#{@@batch_id}/result"
       headers = Hash["Content-Type" => "text/xml; charset=UTF-8"]
