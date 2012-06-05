1aa1b8f02189a5bb28b686ad23bdca325bd8761b
Javier Julio <jjfutbol@gmail.com>
2012-06-05 19:55:21 -0400

    Rewrite initialize test as we need to pass a different set of arguments. Collection needs to know the current result id and the next and previous one rather than be given the whole id set which is unnecessary since fetching the next or previous will just return a new collection instance.

--- test/lib/test_query_result_collection.rb
+++ test/lib/test_query_result_collection.rb
@@ -13,17 +13,29 @@ class TestQueryResultCollection < Test::Unit::TestCase
     @jobId = "123"
     @batchId = "234"
     @resultIds = ["12","23","34"]
+    @resultId = @resultIds[1]
+    @previousResultId = @resultIds.first
+    @nextResultId = @resultIds.last
   end
   
-  test "initilize" do
+  test "initilize using defaults" do
     collection = SalesforceBulk::QueryResultCollection.new(@client, @jobId, @batchId)
     assert_equal collection.client, @client
     assert_equal collection.jobId, @jobId
     assert_equal collection.batchId, @batchId
-    assert_equal collection.currentIndex, 0
-    
-    collection = SalesforceBulk::QueryResultCollection.new(@client, @jobId, @batchId, 5)
-    assert_equal collection.currentIndex, 5
+    assert_equal collection.resultId, nil
+    assert_equal collection.previousResultId, nil
+    assert_equal collection.nextResultId, nil
+  end
+  
+  test "initilize with all values" do
+    collection = SalesforceBulk::QueryResultCollection.new(@client, @jobId, @batchId, @resultId, @previousResultId, @nextResultId)
+    assert_equal collection.client, @client
+    assert_equal collection.jobId, @jobId
+    assert_equal collection.batchId, @batchId
+    assert_equal collection.resultId, @resultId
+    assert_equal collection.previousResultId, @previousResultId
+    assert_equal collection.nextResultId, @nextResultId
   end
   
   test "next?" do
