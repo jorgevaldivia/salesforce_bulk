7772ade6a2bcd94e1bd501e0bc5cd01b54bb5b02
Javier Julio <jjfutbol@gmail.com>
2012-06-05 20:35:37 -0400

    Now we can fix all our variable and method names. Realized to late that I started using camelCase rather than underscore based names. Update tests first.

--- test/lib/test_query_result_collection.rb
+++ test/lib/test_query_result_collection.rb
@@ -10,42 +10,42 @@ class TestQueryResultCollection < Test::Unit::TestCase
     }
     
     @client = SalesforceBulk::Client.new(options)
-    @jobId = "123"
-    @batchId = "234"
-    @resultIds = ["12","23","34"]
-    @totalSize = @resultIds.length
-    @resultId = @resultIds[1]
-    @previousResultId = @resultIds.first
-    @nextResultId = @resultIds.last
-    @collection = SalesforceBulk::QueryResultCollection.new(@client, @jobId, @batchId, @totalSize, @resultId, @previousResultId, @nextResultId)
+    @job_id = "123"
+    @batch_id = "234"
+    @result_ids = ["12","23","34"]
+    @total_size = @result_ids.length
+    @result_id = @result_ids[1]
+    @previous_result_id = @result_ids.first
+    @next_result_id = @result_ids.last
+    @collection = SalesforceBulk::QueryResultCollection.new(@client, @job_id, @batch_id, @total_size, @result_id, @previous_result_id, @next_result_id)
   end
   
   test "initilize using defaults" do
-    collection = SalesforceBulk::QueryResultCollection.new(@client, @jobId, @batchId)
+    collection = SalesforceBulk::QueryResultCollection.new(@client, @job_id, @batch_id)
     
     assert_equal collection.client, @client
-    assert_equal collection.jobId, @jobId
-    assert_equal collection.batchId, @batchId
-    assert_equal collection.totalSize, 0
-    assert_nil collection.resultId
-    assert_nil collection.previousResultId
-    assert_nil collection.nextResultId
+    assert_equal collection.job_id, @job_id
+    assert_equal collection.batch_id, @batch_id
+    assert_equal collection.total_size, 0
+    assert_nil collection.result_id
+    assert_nil collection.previous_result_id
+    assert_nil collection.next_result_id
   end
   
   test "initilize with all values" do
     assert_equal @collection.client, @client
-    assert_equal @collection.jobId, @jobId
-    assert_equal @collection.batchId, @batchId
-    assert_equal @collection.totalSize, @totalSize
-    assert_equal @collection.resultId, @resultId
-    assert_equal @collection.previousResultId, @previousResultId
-    assert_equal @collection.nextResultId, @nextResultId
+    assert_equal @collection.job_id, @job_id
+    assert_equal @collection.batch_id, @batch_id
+    assert_equal @collection.total_size, @total_size
+    assert_equal @collection.result_id, @result_id
+    assert_equal @collection.previous_result_id, @previous_result_id
+    assert_equal @collection.next_result_id, @next_result_id
   end
   
   test "next?" do
     assert @collection.next?
     
-    @collection.instance_variable_set('@nextResultId', '')
+    @collection.instance_variable_set('@next_result_id', '')
     assert !@collection.next?
   end
   
@@ -56,7 +56,7 @@ class TestQueryResultCollection < Test::Unit::TestCase
   test "previous?" do
     assert @collection.previous?
     
-    @collection.instance_variable_set('@previousResultId', '')
+    @collection.instance_variable_set('@previous_result_id', '')
     assert !@collection.previous?
   end
   
