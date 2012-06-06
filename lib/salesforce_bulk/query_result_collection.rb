e944cc79cc236358c055670dafc636a472094f56
Javier Julio <jjfutbol@gmail.com>
2012-06-05 20:36:02 -0400

    Update collection variable and method names to be underscore based and not camelCase. All tests passing.

--- lib/salesforce_bulk/query_result_collection.rb
+++ lib/salesforce_bulk/query_result_collection.rb
@@ -2,39 +2,39 @@ module SalesforceBulk
   class QueryResultCollection < Array
     
     attr_reader :client
-    attr_reader :batchId
-    attr_reader :jobId
-    attr_reader :totalSize
-    attr_reader :resultId
-    attr_reader :previousResultId
-    attr_reader :nextResultId
+    attr_reader :batch_id
+    attr_reader :job_id
+    attr_reader :total_size
+    attr_reader :result_id
+    attr_reader :previous_result_id
+    attr_reader :next_result_id
     
-    def initialize(client, jobId, batchId, totalSize=0, resultId=nil, previousResultId=nil, nextResultId=nil)
+    def initialize(client, job_id, batch_id, total_size=0, result_id=nil, previous_result_id=nil, next_result_id=nil)
       @client = client
-      @jobId = jobId
-      @batchId = batchId
-      @totalSize = totalSize
-      @resultId = resultId
-      @previousResultId = previousResultId
-      @nextResultId = nextResultId
+      @job_id = job_id
+      @batch_id = batch_id
+      @total_size = total_size
+      @result_id = result_id
+      @previous_result_id = previous_result_id
+      @next_result_id = next_result_id
     end
     
     def next?
-      @nextResultId.present?
+      @next_result_id.present?
     end
     
     def next
       # if calls method on client to fetch data and returns new collection instance
-      SalesforceBulk::QueryResultCollection.new(self.client, self.jobId, self.batchId)
+      SalesforceBulk::QueryResultCollection.new(self.client, self.job_id, self.batch_id)
     end
     
     def previous?
-      @previousResultId.present?
+      @previous_result_id.present?
     end
     
     def previous
       # if has previous, calls method on client to fetch data and returns new collection instance
-      SalesforceBulk::QueryResultCollection.new(self.client, self.jobId, self.batchId)
+      SalesforceBulk::QueryResultCollection.new(self.client, self.job_id, self.batch_id)
     end
     
   end
