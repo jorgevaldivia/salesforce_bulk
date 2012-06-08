d8e77a83f796176e48943f5cfc6e8497bc4fd56c
Javier Julio <jjfutbol@gmail.com>
2012-06-08 01:36:40 -0400

    Update to_b method to pass updated test. Only convert if true or false string value is found, otherwise return original value.

--- lib/salesforce_bulk/core_extensions/string.rb
+++ lib/salesforce_bulk/core_extensions/string.rb
@@ -13,6 +13,13 @@ class String
   # For converting "true" and "false" string values returned 
   # by Salesforce Bulk API in batch results to real booleans.
   def to_b
-    present? && lstrip.rstrip.casecmp("true") == 0
+    if present?
+      if lstrip.rstrip.casecmp("true") == 0
+        return true
+      elsif lstrip.rstrip.casecmp("false") == 0
+        return false
+      end
+    end
+    self
   end
 end
