cc778295fd5daea1dbba028c70331f0a940154ce
Javier Julio <jjfutbol@gmail.com>
2012-06-09 19:38:57 -0400

    Add test for the simple API where one method call handles one batch/job setup. I don't think this will really be necessary since the code is pretty much the same across all operations except for query where it would call one other method. For now I'll leave it.

---
+++ test/lib/test_simple_api.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class TestSimpleApi < Test::Unit::TestCase
+  
+  def setup
+    options = {
+      :username => 'myusername', 
+      :password => 'mypassword',
+      :token => "somelongtoken"
+    }
+    
+    @client = SalesforceBulk::Client.new(options)
+  end
+  
+  test "upsert" do
+    @client.expects(:add_job).once.returns(SalesforceBulk::Job.new(@client))
+    @client.expects(:add_batch).once.returns(SalesforceBulk::Batch.new)
+    @client.expects(:close_job).once.returns(SalesforceBulk::Job.new(@client))
+    @client.expects(:batch_info).at_least_once.returns(SalesforceBulk::Batch.new)
+    @client.expects(:batch_result_list).once
+    
+    @client.upsert(:VideoEvent__c, :Id__c, [{:Id__c => '123'}])
+  end
+  
+end
