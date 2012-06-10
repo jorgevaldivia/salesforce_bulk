1875f4e61e12fb0edf0018e960f5860f94f8f763
Javier Julio <jjfutbol@gmail.com>
2012-06-09 20:00:02 -0400

    Update test initialization to cut down on duplicate code by using setup method.

--- test/lib/test_initialization.rb
+++ test/lib/test_initialization.rb
@@ -2,13 +2,21 @@ require 'test_helper'
 
 class TestInitialization < Test::Unit::TestCase
   
-  test "default option values set if none provided" do
-    client = SalesforceBulk::Client.new
+  def setup
+    options = {
+      :username => 'username',
+      :password => 'password',
+      :token => 'token'
+    }
     
-    assert_not_nil SalesforceBulk::Client.new
-    assert_equal client.host, 'login.salesforce.com'
-    assert_equal client.version, '24.0'
-    assert_equal client.debugging, false
+    @client = SalesforceBulk::Client.new(options)
+  end
+  
+  test "default option values set if none provided" do
+    assert_not_nil @client
+    assert_equal @client.host, 'login.salesforce.com'
+    assert_equal @client.version, '24.0'
+    assert_equal @client.debugging, false
   end
   
   test "overriding option defaults and providing login info" do
@@ -32,45 +40,34 @@ class TestInitialization < Test::Unit::TestCase
   end
   
   test "authentication" do
-    options = {
-      :username => 'username',
-      :password => 'password',
-      :token => 'token'
-    }
-    
-    client = SalesforceBulk::Client.new(options)
-    
     headers = {'Content-Type' => 'text/xml', 'SOAPAction' => 'login'}
     request = fixture("login_request.xml")
     response = fixture("login_response.xml")
     
-    stub_request(:post, "https://#{client.host}/services/Soap/u/24.0")
+    stub_request(:post, "https://#{@client.host}/services/Soap/u/24.0")
       .with(:body => request, :headers => headers)
       .to_return(:body => response, :status => 200)
     
-    client.authenticate()
+    @client.authenticate()
     
-    assert_requested :post, "https://#{client.host}/services/Soap/u/24.0", :body => request, :headers => headers, :times => 1
+    assert_requested :post, "https://#{@client.host}/services/Soap/u/24.0", :body => request, :headers => headers, :times => 1
     
-    assert_equal client.instance_host, 'na9-api.salesforce.com'
-    assert_equal client.instance_variable_get('@session_id'), 
-                 '00DE0000000YSKp!AQ4AQNQhDKLMORZx2NwZppuKfure.ChCmdI3S35PPxpNA5MHb3ZVxhYd5STM3euVJTI5.39s.jOBT.3mKdZ3BWFDdIrddS8O'
+    assert_equal @client.instance_host, 'na9-api.salesforce.com'
+    assert_equal @client.instance_variable_get('@session_id'), '00DE0000000YSKp!AQ4AQNQhDKLMORZx2NwZppuKfure.ChCmdI3S35PPxpNA5MHb3ZVxhYd5STM3euVJTI5.39s.jOBT.3mKdZ3BWFDdIrddS8O'
   end
   
   test "parsing instance id from server url" do
-    client = SalesforceBulk::Client.new
-    
-    assert_equal client.instance_id('https://na1-api.salesforce.com'), 'na1-api'
-    assert_equal client.instance_id('https://na23-api.salesforce.com'), 'na23-api'
-    assert_equal client.instance_id('https://na345-api.salesforce.com'), 'na345-api'
+    assert_equal @client.instance_id('https://na1-api.salesforce.com'), 'na1-api'
+    assert_equal @client.instance_id('https://na23-api.salesforce.com'), 'na23-api'
+    assert_equal @client.instance_id('https://na345-api.salesforce.com'), 'na345-api'
     
     # protocol shouldn't matter, its just part of the host name we are after
-    assert_equal client.instance_id('://na1-api.salesforce.com'), 'na1-api'
-    assert_equal client.instance_id('://na23-api.salesforce.com'), 'na23-api'
+    assert_equal @client.instance_id('://na1-api.salesforce.com'), 'na1-api'
+    assert_equal @client.instance_id('://na23-api.salesforce.com'), 'na23-api'
     
     # in fact the .com portion shouldn't matter either
-    assert_equal client.instance_id('://na1-api.salesforce'), 'na1-api'
-    assert_equal client.instance_id('://na23-api.salesforce'), 'na23-api'
+    assert_equal @client.instance_id('://na1-api.salesforce'), 'na1-api'
+    assert_equal @client.instance_id('://na23-api.salesforce'), 'na23-api'
   end
   
 end
