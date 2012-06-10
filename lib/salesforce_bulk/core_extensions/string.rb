0e00fbd0b721b40dc174c5f3f129202525ba1fd3
Javier Julio <jjfutbol@gmail.com>
2012-06-10 12:03:11 -0400

    Remove encoding aware from string extension as we'll be including the active_support gem anyway.

--- lib/salesforce_bulk/core_extensions/string.rb
+++ lib/salesforce_bulk/core_extensions/string.rb
@@ -1,15 +1,4 @@
 class String
-  # From ActiveSupport gem: /activesupport/lib/active_support/core_ext/string/encoding.rb
-  if defined?(Encoding) && "".respond_to?(:encode)
-    def encoding_aware?
-      true
-    end
-  else
-    def encoding_aware?
-      false
-    end
-  end
-  
   # For converting "true" and "false" string values returned 
   # by Salesforce Bulk API in batch results to real booleans.
   def to_b
