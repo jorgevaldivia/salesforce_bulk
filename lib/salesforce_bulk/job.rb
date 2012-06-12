f81f7cf5eea3ffdc2514d19703f79a557f6d75e7
Javier Julio <jjfutbol@gmail.com>
2012-06-11 20:27:43 -0400

    Add commented out job info XML to job class so we know what properties to create.

--- lib/salesforce_bulk/job.rb
+++ lib/salesforce_bulk/job.rb
@@ -7,6 +7,28 @@ module SalesforceBulk
     attr_reader :operation
     attr_reader :sobject
     attr_accessor :state
+#  <id>750E00000004N1mIAE</id>
+#  <operation>upsert</operation>
+#  <object>VideoEvent__c</object>
+#  <createdById>005E00000017spfIAA</createdById>
+#  <createdDate>2012-05-30T04:08:30.000Z</createdDate>
+#  <systemModstamp>2012-05-30T04:08:30.000Z</systemModstamp>
+#  <state>Open</state>
+#  <externalIdFieldName>Id__c</externalIdFieldName>
+#  <concurrencyMode>Parallel</concurrencyMode>
+#  <contentType>CSV</contentType>
+#  <numberBatchesQueued>0</numberBatchesQueued>
+#  <numberBatchesInProgress>0</numberBatchesInProgress>
+#  <numberBatchesCompleted>0</numberBatchesCompleted>
+#  <numberBatchesFailed>0</numberBatchesFailed>
+#  <numberBatchesTotal>0</numberBatchesTotal>
+#  <numberRecordsProcessed>0</numberRecordsProcessed>
+#  <numberRetries>0</numberRetries>
+#  <apiVersion>24.0</apiVersion>
+#  <numberRecordsFailed>0</numberRecordsFailed>
+#  <totalProcessingTime>0</totalProcessingTime>
+#  <apiActiveProcessingTime>0</apiActiveProcessingTime>
+#  <apexProcessingTime>0</apexProcessingTime>
     
     def initialize(options={})
       @operation = options[:operation]
