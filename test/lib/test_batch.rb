4b2318b1020d5ba92d9272704b9fdd73fbba364e
Javier Julio <jjfutbol@gmail.com>
2012-06-13 11:27:35 -0400

    Update match tests when requesting batch info to use and verify headers contains XML content type.

--- test/lib/test_batch.rb
+++ test/lib/test_batch.rb
@@ -87,11 +87,11 @@ class TestBatch < Test::Unit::TestCase
     job_id = "750E00000004N97IAE"
     
     bypass_authentication(@client)
-    stub_request(:get, "#{api_url(@client)}job/#{job_id}/batch").to_return(:body => response, :status => 200)
+    stub_request(:get, "#{api_url(@client)}job/#{job_id}/batch").with(:headers => @headersWithXml).to_return(:body => response, :status => 200)
     
     batches = @client.batch_info_list(job_id)
     
-    assert_requested :get, "#{api_url(@client)}job/#{job_id}/batch", :times => 1
+    assert_requested :get, "#{api_url(@client)}job/#{job_id}/batch", :headers => @headersWithXml, :times => 1
     
     assert_kind_of Array, batches
     assert_kind_of SalesforceBulk::Batch, batches.first
@@ -110,12 +110,12 @@ class TestBatch < Test::Unit::TestCase
     
     bypass_authentication(@client)
     stub_request(:get, "#{api_url(@client)}job/#{job_id}/batch/#{batch_id}")
-      .with(:headers => @headers)
+      .with(:headers => @headersWithXml)
       .to_return(:body => response, :status => 200)
     
     batch = @client.batch_info(job_id, batch_id)
     
-    assert_requested :get, "#{api_url(@client)}job/#{job_id}/batch/#{batch_id}", :headers => @headers, :times => 1
+    assert_requested :get, "#{api_url(@client)}job/#{job_id}/batch/#{batch_id}", :headers => @headersWithXml, :times => 1
     
     assert_equal batch.job_id, job_id
     assert_equal batch.state, 'Completed'
