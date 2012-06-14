3965fc4072d72f48ee095631f14d6e9b7d396186
Javier Julio <jjfutbol@gmail.com>
2012-06-13 20:25:57 -0400

    Updated batch_result class to pass all tests.

--- lib/salesforce_bulk/batch_result.rb
+++ lib/salesforce_bulk/batch_result.rb
@@ -13,12 +13,27 @@ module SalesforceBulk
     # If record was created successfully. If false then an error message is provided. 
     attr_accessor :success
     
-    def initialize(attrs={})
-      
+    def initialize(id, success, created, error)
+      @id = id
+      @success = success
+      @created = created
+      @error = error
     end
     
     def error?
       error.present?
+    end
+    
+    def created?
+      created
+    end
+    
+    def successful?
+      success
+    end
+    
+    def updated?
+      !created && success
     end
   end
 end
