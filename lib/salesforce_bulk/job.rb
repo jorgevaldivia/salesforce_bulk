db016e578714fc6e0ea1f62a02fd4ffe2cab4090
Javier Julio <jjfutbol@gmail.com>
2012-06-14 02:57:59 -0400

    Add necessary new fields to Job model and add commented out fields.

--- lib/salesforce_bulk/job.rb
+++ lib/salesforce_bulk/job.rb
@@ -6,7 +6,24 @@ module SalesforceBulk
     attr_accessor :id
     attr_reader :operation
     attr_reader :sobject
+    attr_accessor :job_id
     attr_accessor :state
+    attr_accessor :created_by
+    attr_accessor :created_at
+    attr_accessor :updated_at
+    attr_accessor :content_type
+    attr_accessor :queued_batches
+    attr_accessor :in_progress_batches
+    attr_accessor :completed_batches
+    attr_accessor :failed_batches
+    attr_accessor :total_batches
+    attr_accessor :processed_batches
+    attr_accessor :retries
+    attr_accessor :api_version
+    attr_accessor :failed_records
+    attr_accessor :apex_processing_time
+    attr_accessor :api_active_processing_time
+    attr_accessor :total_processing_time
 #  <id>750E00000004N1mIAE</id>
 #  <operation>upsert</operation>
 #  <object>VideoEvent__c</object>
@@ -29,6 +46,30 @@ module SalesforceBulk
 #  <totalProcessingTime>0</totalProcessingTime>
 #  <apiActiveProcessingTime>0</apiActiveProcessingTime>
 #  <apexProcessingTime>0</apexProcessingTime>
+    
+    def self.new_from_xml(data)
+      job = data['id']
+      job.job_id = data['jobId']
+      job.operation = data['operation']
+      job.created_by = data['createdById']
+      job.state = data['state']
+      job.started_at = DateTime.parse(data['createdDate'])
+      job.ended_at = DateTime.parse(data['systemModstamp'])
+      job.external_id_field_name = data['externalIdFieldName']
+      job.concurrency_mode = data['concurrencyMode']
+      job.content_type = data['contentType']
+      job.queued_batches = data['numberBatchesQueued'].to_i
+      job.in_progress_batches = data['numberBatchesInProgress'].to_i
+      job.completed_batches = data['numberBatchesCompleted'].to_i
+      job.failed_batches = data['numberBatchesFailed'].to_i
+      job.processed_records = data['numberRecordsProcessed'].to_i
+      job.retries = data['retries'].to_i
+      job.failed_records = data['numberRecordsFailed'].to_i
+      job.total_processing_time = data['totalProcessingTime'].to_i
+      job.api_active_processing_time = data['apiActiveProcessingTime'].to_i
+      job.apex_processing_time = data['apex_processing_time'].to_i
+      job
+    end
     
     def initialize(attrs={})
       @operation = attrs['operation']
