9eebcad49b9869d995d1203b56b86df34af8bb4f
Javier Julio <jjfutbol@gmail.com>
2012-06-14 23:12:48 -0400

    Update job class with all necessary attr_accessors and update new_from_xml to pass test.

--- lib/salesforce_bulk/job.rb
+++ lib/salesforce_bulk/job.rb
@@ -1,60 +1,38 @@
 module SalesforceBulk
   class Job
     
-    attr_reader :concurrency_mode
-    attr_reader :external_id_field_name
+    attr_accessor :concurrency_mode
+    attr_accessor :external_id_field_name
     attr_accessor :id
-    attr_reader :operation
-    attr_reader :sobject
-    attr_accessor :job_id
+    attr_accessor :operation
+    attr_accessor :sobject
     attr_accessor :state
     attr_accessor :created_by
     attr_accessor :created_at
-    attr_accessor :updated_at
+    attr_accessor :completed_at
     attr_accessor :content_type
     attr_accessor :queued_batches
     attr_accessor :in_progress_batches
     attr_accessor :completed_batches
     attr_accessor :failed_batches
     attr_accessor :total_batches
-    attr_accessor :processed_batches
     attr_accessor :retries
-    attr_accessor :api_version
     attr_accessor :failed_records
+    attr_accessor :processed_records
     attr_accessor :apex_processing_time
     attr_accessor :api_active_processing_time
     attr_accessor :total_processing_time
-#  <id>750E00000004N1mIAE</id>
-#  <operation>upsert</operation>
-#  <object>VideoEvent__c</object>
-#  <createdById>005E00000017spfIAA</createdById>
-#  <createdDate>2012-05-30T04:08:30.000Z</createdDate>
-#  <systemModstamp>2012-05-30T04:08:30.000Z</systemModstamp>
-#  <state>Open</state>
-#  <externalIdFieldName>Id__c</externalIdFieldName>
-#  <concurrencyMode>Parallel</concurrencyMode>
-#  <contentType>CSV</contentType>
-#  <numberBatchesQueued>0</numberBatchesQueued>
-#  <numberBatchesInProgress>0</numberBatchesInProgress>
-#  <numberBatchesCompleted>0</numberBatchesCompleted>
-#  <numberBatchesFailed>0</numberBatchesFailed>
-#  <numberBatchesTotal>0</numberBatchesTotal>
-#  <numberRecordsProcessed>0</numberRecordsProcessed>
-#  <numberRetries>0</numberRetries>
-#  <apiVersion>24.0</apiVersion>
-#  <numberRecordsFailed>0</numberRecordsFailed>
-#  <totalProcessingTime>0</totalProcessingTime>
-#  <apiActiveProcessingTime>0</apiActiveProcessingTime>
-#  <apexProcessingTime>0</apexProcessingTime>
+    attr_accessor :api_version
     
     def self.new_from_xml(data)
-      job = data['id']
-      job.job_id = data['jobId']
+      job = self.new
+      job.id = data['id']
       job.operation = data['operation']
+      job.sobject = data['object']
       job.created_by = data['createdById']
       job.state = data['state']
-      job.started_at = DateTime.parse(data['createdDate'])
-      job.ended_at = DateTime.parse(data['systemModstamp'])
+      job.created_at = DateTime.parse(data['createdDate'])
+      job.completed_at = DateTime.parse(data['systemModstamp'])
       job.external_id_field_name = data['externalIdFieldName']
       job.concurrency_mode = data['concurrencyMode']
       job.content_type = data['contentType']
@@ -62,12 +40,14 @@ module SalesforceBulk
       job.in_progress_batches = data['numberBatchesInProgress'].to_i
       job.completed_batches = data['numberBatchesCompleted'].to_i
       job.failed_batches = data['numberBatchesFailed'].to_i
-      job.processed_records = data['numberRecordsProcessed'].to_i
+      job.total_batches = data['totalBatches'].to_i
       job.retries = data['retries'].to_i
+      job.processed_records = data['numberRecordsProcessed'].to_i
       job.failed_records = data['numberRecordsFailed'].to_i
       job.total_processing_time = data['totalProcessingTime'].to_i
       job.api_active_processing_time = data['apiActiveProcessingTime'].to_i
-      job.apex_processing_time = data['apex_processing_time'].to_i
+      job.apex_processing_time = data['apexProcessingTime'].to_i
+      job.api_version = data['apiVersion'].to_i
       job
     end
     
