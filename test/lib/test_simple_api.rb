675df8d18ce0ae8d032312d7945e962c2ed72ae8
Javier Julio <jjfutbol@gmail.com>
2012-06-14 23:46:38 -0400

    Remove concurrency_mode support as its unnecessary with the basic single operation API since there's only ever 1 batch created.

--- test/lib/test_simple_api.rb
+++ test/lib/test_simple_api.rb
@@ -67,7 +67,7 @@ class TestSimpleApi < Test::Unit::TestCase
   test "upsert" do
     data = [{:Id__c => '123123', :Title__c => 'Test Title'}, {:Id__c => '234234', :Title__c => 'A Second Title'}]
     
-    @client.expects(:add_job).once.with(:upsert, :VideoEvent__c, :concurrency_mode => nil, :external_id_field_name => :Id__c).returns(@job)
+    @client.expects(:add_job).once.with(:upsert, :VideoEvent__c, :external_id_field_name => :Id__c).returns(@job)
     @client.expects(:add_batch).once.with(@job.id, data).returns(@batch)
     @client.expects(:close_job).once.with(@job.id).returns(@job)
     @client.expects(:batch_info).at_least_once.returns(@batch)
