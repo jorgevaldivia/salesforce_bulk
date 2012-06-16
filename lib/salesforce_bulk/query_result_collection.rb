7851f0a3203461175a7416e8cc2686105a19155b
Javier Julio <jjfutbol@gmail.com>
2012-06-15 20:58:38 -0400

    Update next method to now call client.query_result if a next set is available passing in all the required values where the one change is that for result_id the next result_id in the set is provided. All tests passing.

--- lib/salesforce_bulk/query_result_collection.rb
+++ lib/salesforce_bulk/query_result_collection.rb
@@ -21,8 +21,7 @@ module SalesforceBulk
     end
     
     def next
-      # if calls method on client to fetch data and returns new collection instance
-      SalesforceBulk::QueryResultCollection.new(self.client, self.job_id, self.batch_id)
+      @client.query_result(job_id, batch_id, result_ids[@current_index + 1], result_ids) if next?
     end
     
     def previous?
