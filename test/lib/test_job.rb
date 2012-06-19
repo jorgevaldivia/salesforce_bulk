195257bdde53ce29c0cb1fc7cac0bba362f96d0c
Javier Julio <jjfutbol@gmail.com>
2012-06-19 01:00:05 -0400

    Update test for close_job method to assert all values returned in response. We want to use the Job.new_from_xml method in the response now.

--- test/lib/test_job.rb
+++ test/lib/test_job.rb
@@ -126,18 +126,38 @@ class TestJob < Test::Unit::TestCase
   test "should close job and return successful response" do
     request = fixture("job_close_request.xml")
     response = fixture("job_close_response.xml")
-    jobId = "750E00000004MzbIAE"
+    job_id = "750E00000004MzbIAE"
     
-    stub_request(:post, "#{api_url(@client)}job/#{jobId}")
+    stub_request(:post, "#{api_url(@client)}job/#{job_id}")
       .with(:body => request, :headers => @headers)
       .to_return(:body => response, :status => 200)
     
-    job = @client.close_job(jobId)
+    job = @client.close_job(job_id)
     
-    assert_requested :post, "#{api_url(@client)}job/#{jobId}", :body => request, :headers => @headers, :times => 1
+    assert_requested :post, "#{api_url(@client)}job/#{job_id}", :body => request, :headers => @headers, :times => 1
     
-    assert_equal job.id, jobId
+    assert_equal job.id, job_id
+    assert_equal job.operation, 'upsert' 
+    assert_equal job.sobject, 'VideoEvent__c'
+    assert_equal job.created_by, '005E00000017spfIAA'
+    assert_equal job.created_at, DateTime.parse('2012-05-29T23:51:53.000Z')
+    assert_equal job.completed_at, DateTime.parse('2012-05-29T23:51:53.000Z')
     assert_equal job.state, 'Closed'
+    assert_equal job.external_id_field_name, 'Id__c'
+    assert_equal job.concurrency_mode, 'Parallel'
+    assert_equal job.content_type, 'CSV'
+    assert_equal job.queued_batches, 0
+    assert_equal job.in_progress_batches, 0
+    assert_equal job.completed_batches, 0
+    assert_equal job.failed_batches, 0
+    assert_equal job.total_batches, 0
+    assert_equal job.processed_records, 0
+    assert_equal job.failed_records, 0
+    assert_equal job.retries, 0
+    assert_equal job.api_active_processing_time, 0
+    assert_equal job.apex_processing_time, 0
+    assert_equal job.total_processing_time, 0
+    assert_equal job.api_version, 24.0
   end
   
   test "should abort job and return successful response" do
