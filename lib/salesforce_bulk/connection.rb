b8a8b8874f3f1afcd385e2a51c4e613ab41c455a
Leif Gensert <leif@propertybase.com>
2013-04-11 11:59:43 +0200

    refactor connection to use new http api

--- lib/salesforce_bulk/connection.rb
+++ lib/salesforce_bulk/connection.rb
@@ -1,7 +1,5 @@
 module SalesforceBulk
   class Connection
-    LOGIN_HOST = 'login.salesforce.com'
-
     attr_reader :instance
     attr_reader :session_id
     attr_reader :api_version
@@ -15,46 +13,34 @@ module SalesforceBulk
     end
 
     def login
-      r = SalesforceBulk::Http::Request.create_login(
+      response = SalesforceBulk::Http.login(
         @sandbox,
         @username,
         @password,
         @api_version)
 
-      response = request r
-      response_parsed = parse_response response
+      puts "the login response: #{response.inspect}"
 
-      @session_id = response_parsed['Body'][0]['loginResponse'][0]['result'][0]['sessionId'][0]
-      @server_url = response_parsed['Body'][0]['loginResponse'][0]['result'][0]['serverUrl'][0]
-      @instance = parse_instance()
+      @session_id = response[:session_id]
+      @instance = response[:instance]
     end
 
     def create_job operation, sobject, external_field
-      response = request(SalesforceBulk::Http::Request.create_job(
+      SalesforceBulk::Http.create_job(
         @instance,
         @session_id,
         operation,
         sobject,
         @api_version,
-        external_field))
-
-      response_parsed = parse_response response
-      response_parsed['id'][0]
+        external_field)[:id]
     end
 
     def close_job job_id
-      response = request(SalesforceBulk::Http::Request.close_job(
+      SalesforceBulk::Http.close_job(
         @instance,
         @session_id,
         job_id,
-        @api_version))
-
-      puts "====="
-      puts "close_job response: #{response.inspect}"
-      puts "====="
-
-      response_parsed = parse_response response
-      response_parsed['id'][0]
+        @api_version)[:id]
     end
 
     def add_batch job_id, records
@@ -69,66 +55,12 @@ module SalesforceBulk
         rows << fields.to_csv
       end
 
-      r = SalesforceBulk::Http::Request.add_batch(
+      SalesforceBulk::Http.add_batch(
         @instance,
         @session_id,
         job_id,
         rows,
-        @api_version)
-
-      response = request(r)
-
-      puts '+++++'
-      puts "headers of request: #{r.headers.inspect}"
-      puts "batch: #{response.inspect}"
-      puts '+++++'
-      response_parsed = XmlSimple.xml_in(response)
-
-      response_parsed['id'][0]
-    end
-
-    def request r
-      puts r.inspect
-      https(r.host).post(r.path, r.body, r.headers).body
-    end
-
-    def get_request(host, path, headers)
-      https(host).get(path, headers).body
-    end
-
-    def https(host)
-      req = Net::HTTP.new(host, 443)
-      req.use_ssl = true
-      req.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      req
-    end
-
-    def parse_instance
-      #TODO make more ruby like
-      @server_url =~ /https:\/\/([a-z]{2,2}[0-9]{1,2})-api/
-      @instance = $~.captures[0]
-    end
-
-    def parse_response response
-      response_parsed = XmlSimple.xml_in(response)
-
-      if response.downcase.include?('faultstring') || response.downcase.include?('exceptionmessage')
-        begin
-          
-          if response.downcase.include?('faultstring')
-            error_message = response_parsed['Body'][0]['Fault'][0]['faultstring'][0]
-          elsif response.downcase.include?('exceptionmessage')
-            error_message = response_parsed['exceptionMessage'][0]
-          end
-
-        rescue
-          raise "An unknown error has occured within the salesforce_bulk gem. This is most likely caused by bad request, but I am unable to parse the correct error message. Here is a dump of the response for your convenience. #{response}"
-        end
-
-        raise error_message
-      end
-
-      response_parsed
+        @api_version)[:id]
     end
   end
 end
