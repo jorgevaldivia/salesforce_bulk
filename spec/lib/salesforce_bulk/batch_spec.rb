6d2de8db844697135301a75567f7d120774867eb
Leif Gensert <leif@propertybase.com>
2013-04-21 11:13:43 +0200

    add test for batch#final_status to test -1 batch_id

--- spec/lib/salesforce_bulk/batch_spec.rb
+++ spec/lib/salesforce_bulk/batch_spec.rb
@@ -10,5 +10,15 @@ describe SalesforceBulk::Batch do
       b.instance_variable_set '@final_status', expected_status
       expect(b.final_status).to eq(expected_status)
     end
+
+    it 'should return specific final status for -1 batch id' do
+      b = described_class.new nil, nil, -1
+      expected_status = {
+          state: 'Completed',
+          state_message: 'Empty Request'
+        }
+      b.should_not_receive(:status)
+      expect(b.final_status).to eq(expected_status)
+    end
   end
 end
