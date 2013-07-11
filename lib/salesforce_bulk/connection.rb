bccf586a787c380d46e5bbd8102f59dd06c4978f
jorgevaldivia <jorge+dev@valdivia.me>
2013-07-11 15:17:57 -0500

    check for custom subdomains in salesforce instance parse

--- lib/salesforce_bulk/connection.rb
+++ lib/salesforce_bulk/connection.rb
@@ -83,7 +83,13 @@ module SalesforceBulk
     def parse_instance()
       @server_url =~ /https:\/\/([a-z]{2,2}[0-9]{1,2})(-api)?/
       if $~.nil?
-        raise "Unable to parse Salesforce instance from server url (#{@server_url})."
+        # Check for a "My Domain" subdomain
+        @server_url =~ /https:\/\/[a-zA-Z\-0-9]*.([a-z]{2,2}[0-9]{1,2})(-api)?/
+        if $~.nil?
+          raise "Unable to parse Salesforce instance from server url (#{@server_url})."
+        else
+          @instance = $~.captures[0]
+        end
       else
         @instance = $~.captures[0]
       end
