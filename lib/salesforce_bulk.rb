81a938f1ac6339448b30b2f39e5f41856d488a1c
Nathan Woodhull <woodhull@gmail.com>
2013-11-14 16:32:36 -0500

    increase api version.

--- lib/salesforce_bulk.rb
+++ lib/salesforce_bulk.rb
@@ -9,7 +9,7 @@ module SalesforceBulk
   # Your code goes here...
   class Api
 
-    SALESFORCE_API_VERSION = '24.0'
+    SALESFORCE_API_VERSION = '29.0'
 
     def initialize(username, password, in_sandbox=false)
       @connection = SalesforceBulk::Connection.new(username, password, SALESFORCE_API_VERSION, in_sandbox)
